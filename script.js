89e084597d9356906e97e53e2a518fb5775d2db0
JhonnM62 <jonnymejia62@gmail.com>
2023-08-19 10:00:40 -0500

    Extencion para sincronizar app de appsheet(ya logra dar click pero no cada 5 seg)

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-console.log("Extensión de sincronización automática");
-document.addEventListener("DOMContentLoaded", function () {
-  const syncbutton = document.getElementById("inicioS");
-  syncbutton.addEventListener("click", clickButton);
+function injectTheScript(scriptFunction) {
+  // Wuery the active tab, which will be only one tab and inject the script function in it.
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      func: scriptFunction,
+    });
+  });
+}
 
-  function clickButton() {
-    console.log("click");
-
-    const button = document.querySelector(
-      'button[data-testid="SyncActionButton"]'
-    );
-    if (button) {
-      button.click();
-    }
+function clickButton() {
+  const button = document.querySelector(
+    'button[data-testid="SyncActionButton"]'
+  );
+  if (button) {
+    button.click();
   }
+}
+
+document.getElementById("inicioS").addEventListener("click", () => {
+  console.log("Button clicked");
+  injectTheScript(clickButton);
   setInterval(clickButton, 5000);
 });
